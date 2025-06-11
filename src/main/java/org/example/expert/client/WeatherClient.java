37a65b5a173b5892392569143aa1cdbab59a0a45
taehunim <luxlaetitia9542@gmail.com>
2025-06-11 16:10:40 +0900

    level 1-2 리팩토링 퀴즈 - 불필요한 if-else 피하기

--- src/main/java/org/example/expert/client/WeatherClient.java
+++ src/main/java/org/example/expert/client/WeatherClient.java
@@ -25,38 +25,42 @@ public class WeatherClient {
     public String getTodayWeather() {
         ResponseEntity<WeatherDto[]> responseEntity =
                 restTemplate.getForEntity(buildWeatherApiUri(), WeatherDto[].class);
-
         WeatherDto[] weatherArray = responseEntity.getBody();
         if (!HttpStatus.OK.equals(responseEntity.getStatusCode())) {
             throw new ServerException("날씨 데이터를 가져오는데 실패했습니다. 상태 코드: " + responseEntity.getStatusCode());
-        } else {
-            if (weatherArray == null || weatherArray.length == 0) {
-                throw new ServerException("날씨 데이터가 없습니다.");
-            }
         }
 
-        String today = getCurrentDate();
-
-        for (WeatherDto weatherDto : weatherArray) {
-            if (today.equals(weatherDto.getDate())) {
-                return weatherDto.getWeather();
-            }
+        if (weatherArray == null || weatherArray.length == 0) {
+            throw new ServerException("날씨 데이터가 없습니다.");
         }
 
-        throw new ServerException("오늘에 해당하는 날씨 데이터를 찾을 수 없습니다.");
+    String today = getCurrentDate();
+
+        for(
+    WeatherDto weatherDto :weatherArray)
+
+    {
+        if (today.equals(weatherDto.getDate())) {
+            return weatherDto.getWeather();
+        }
     }
 
-    private URI buildWeatherApiUri() {
-        return UriComponentsBuilder
-                .fromUriString("https://f-api.github.io")
-                .path("/f-api/weather.json")
-                .encode()
-                .build()
-                .toUri();
-    }
+        throw new
 
-    private String getCurrentDate() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd");
-        return LocalDate.now().format(formatter);
-    }
+    ServerException("오늘에 해당하는 날씨 데이터를 찾을 수 없습니다.");
+}
+
+private URI buildWeatherApiUri() {
+    return UriComponentsBuilder
+            .fromUriString("https://f-api.github.io")
+            .path("/f-api/weather.json")
+            .encode()
+            .build()
+            .toUri();
+}
+
+private String getCurrentDate() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd");
+    return LocalDate.now().format(formatter);
+}
 }
