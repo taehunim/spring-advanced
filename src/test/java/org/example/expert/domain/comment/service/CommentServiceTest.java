052f97f91d1f7425ee042e84e19177550a78d8f5
taehunim <luxlaetitia9542@gmail.com>
2025-06-12 10:17:11 +0900

    테스트 코드 연습 - 2

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -5,11 +5,13 @@ import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
 import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -43,12 +45,14 @@ class CommentServiceTest {
         given(todoRepository.findById(anyLong())).willReturn(Optional.empty());
 
         // when
-        ServerException exception = assertThrows(ServerException.class, () -> {
-            commentService.saveComment(authUser, todoId, request);
-        });
+
+        InvalidRequestException exception = assertThrows(
+                InvalidRequestException.class,
+                () -> commentService.saveComment(authUser, todoId, request)
+        );
 
         // then
-        assertEquals("Todo not found", exception.getMessage());
+        org.assertj.core.api.Assertions.assertThat(exception.getMessage()).isEqualTo("Todo not found");
     }
 
     @Test
